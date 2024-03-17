655b6b899579b3facda9905ddab6a859b3e9e7b6
Deepp805 <deeppatel1944@gmail.com>
2024-03-17 14:17:43 -0400

    migrating from websockets to socket.io

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import { createServer } from "http"
+import { Server } from "socket.io"
+
+const httpServer = createServer()
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"]
+    }
+})
+
+server.on('connection', socket => {
+    socket.on('message', message => {
+        const b = Buffer.from(message)
+        console.log(b.toString())
+        socket.send(`${message}`)
+    })
+})
